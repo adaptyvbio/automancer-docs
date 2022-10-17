5171514e2bb8f4ed126374484bc67843d950da17
Simon Liétar <simon@lietar.net>
2022-10-17 23:37:31 +0200

    Improve contents

---
+++ src/shared.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+
+// export const Brand = 'PR–1';
+// export const LangBrand = 'PCRL';
+
+export function Brand() {
+  return <>PR–1</>;
+}
+
+export function LangBrand() {
+  return <>PCRL</>;
+}
